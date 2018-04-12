0c01ccfc7dd0018480f4cfcf98550c54ff8b967c
Noreh AD <akinoreh@gmail.com>
2018-04-12 15:30:46 +0300

    .

---
+++ globals.php
@@ -0,0 +1,33 @@
+<?php
+    
+    /*
+    
+    http://php.net/manual/en/reserved.variables.globals.php
+    
+    $GLOBALS = [
+        
+        "_GET"      => [],
+        
+        "_POST"     => [],
+        
+        "_COOKIE"   => [],
+        
+        "_FILES"    => [],
+        
+        "GLOBALS"   => [],
+        
+        ...
+        
+    ];
+    
+    $_SERVER  = [];
+    
+    $_REQUEST = [];
+    
+    $_SESSION = [];
+    
+    ...
+    
+    */
+    
+?>
