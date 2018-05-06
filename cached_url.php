67a5251a884ce95d207d2088d80f5038bfac620a
Noreh AD <akinoreh@gmail.com>
2018-05-06 20:34:56 +0300

    is_online()

--- cached_url.php
+++ cached_url.php
@@ -1,24 +1,31 @@
 <?php
     
-    function is_recent($filepath, $age) {
+    function is_online() {
+        if (!@fopen("http://example.com/", "r")) {
+            return false;
+        }
+        return true;
+    }
+    
+    function is_recent($filepath, $max_age) {
         $then = filemtime($filepath);
         $now  = time();
         $diff = $now - $then;
-        if ($diff < $age) {
+        if ($diff < $max_age) {
             return true;
         }
         return false;
     }
     
-    function cached_url($url, $filepath, $age) {
+    function cached_url($url, $filepath, $max_age) {
         $content = null;
         if (file_exists($filepath)) {
-            if (is_recent($filepath, $age)) {
-                $content = file_get_contents($filepath);
-            } else {
+            if (!is_recent($filepath, $max_age) && is_online()) {
                 unlink($filepath);
                 $content = file_get_contents($url);
                 file_put_contents($filepath, $content);
+            } else {
+                $content = file_get_contents($filepath);
             }
         } else {
             $content = file_get_contents($url);
@@ -27,27 +34,27 @@
         return $content;
     }
     
-    function cached_parsed_url($url, $raw_path, $cached_path, $age, $parse_callback) {
+    function cached_parsed_url($url, $raw_path, $cached_path, $max_age, $parse_callback) {
         $raw_content    = null;
         $cached_content = null;
         if (file_exists($cached_path)) {
-            if (is_recent($cached_path, $age)) {
-                $cached_content = file_get_contents($cached_path);
-            } else {
+            if (!is_recent($cached_path, $max_age) && is_online()) {
                 @unlink($raw_path);
                 unlink($cached_path);
                 $raw_content = file_get_contents($url);
                 file_put_contents($raw_path, $raw_content);
                 $cached_content = $parse_callback($raw_content);
                 file_put_contents($cached_path, $cached_content);
+            } else {
+                $cached_content = file_get_contents($cached_path);
             }
         } else {
             if (file_exists($raw_path)) {
-                if (is_recent($raw_path, $age)) {
-                    $raw_content = file_get_contents($raw_path);
-                } else {
+                if (!is_recent($raw_path, $max_age) && is_online()) {
                     $raw_content = file_get_contents($url);
                     file_put_contents($raw_path, $raw_content);
+                } else {
+                    $raw_content = file_get_contents($raw_path);
                 }
             } else {
                 $raw_content = file_get_contents($url);
