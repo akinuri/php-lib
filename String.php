82ec3fcce5430d17026d196472b56b632f7731c4
Noreh AD <akinoreh@gmail.com>
2018-08-16 18:04:36 +0300

    .

--- String.php
+++ String.php
@@ -24,4 +24,27 @@
         return false;
     }
     
+    function latinize($text) {
+        $chars = [
+            "ç", "ğ", "ı", "ö", "ş", "ü",
+            "Ç", "Ğ", "İ", "Ö", "Ş", "Ü",
+        ];
+        $replacements = [
+            "c", "g", "i", "o", "s", "u",
+            "C", "G", "I", "O", "S", "U",
+        ];
+        for ($i = 0; $i < count($chars); $i++) {
+            $char        = $chars[$i];
+            $replacement = $chars[$i];
+            $text = str_replace($char, $replacement, $text);
+        }
+        return $text;
+    }
+    
+    function str_starts_with($haystack, $needle) {
+        return $haystack[0] === $needle[0]
+            ? strncmp($haystack, $needle, strlen($needle)) === 0
+            : false;
+    }
+    
 ?>
