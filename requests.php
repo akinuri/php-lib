6cb169200c4ead6b518d407c11c9a95bc66d30cd
Noreh AD <akinoreh@gmail.com>
2019-02-20 11:43:41 +0300

    .

--- requests.php
+++ requests.php
@@ -1,52 +1,47 @@
 <?php
-    
-    function GET($param, $switchlike = false, $required = false) {
-        
-        $value = null;
-        
-        if (isset($_GET[$param])) {
-            
-            $value = $_GET[$param];
-            
+
+function GET($param, $switchlike = false, $required = false) {
+    $value = null;
+    if (isset($_GET[$param])) {
+        $value = $_GET[$param];
+        if ($switchlike && in_array(strtolower($value), ["1", "on", "true", ""])) {
+            return true;
+        }
+        if ($required && trim($value) == "") {
+            die(json_encode(["error" => "Missing parameter value: {$param}"]));
+        }
+        return $value;
+    }
+    if ($required) {
+        die(json_encode(["error" => "Missing parameter: {$param}"]));
+    }
+    return null;
+}
+
+function POST($param, $required = false) {
+    $value = null;
+    if (isset($_POST[$param])) {
+        $value = $_POST[$param];
+    }
+    else if (isset($_FILES[$param])) {
+        $value = $_FILES[$param];
+    }
+    if ($value) {
+        if (is_string($value)) {
             if ($switchlike && in_array(strtolower($value), ["1", "on", "true", ""])) {
                 return true;
             }
-            
             if ($required && trim($value) == "") {
-                die(json_encode(["error" => "Missing parameter value: $param"]));
+                die(json_encode(["error" => "Missing parameter value: {$param}"]));
             }
-            
             return $value;
         }
-        
-        if ($required) {
-            die(json_encode(["error" => "Missing parameter: $param"]));
+        if (is_array($value)) {
+            return $value;
         }
-        
-        return null;
     }
-    
-    function POST($param, $required = false) {
-        $value = null;
-        if (isset($_POST[$param])) {
-            $value = $_POST[$param];
-        } else if (isset($_FILES[$param])) {
-            $value = $_FILES[$param];
-        }
-        if ($value) {
-            if (is_string($value)) {
-                if ($required && trim($value) == "") {
-                    die(json_encode(["error" => "Missing parameter value: $param"]));
-                }
-                return $value;
-            }
-            if (is_array($value)) {
-                return $value;
-            }
-        }
-        if ($required) {
-            die(json_encode(["error" => "Missing parameter: $param"]));
-        }
-        return null;
+    if ($required) {
+        die(json_encode(["error" => "Missing parameter: {$param}"]));
     }
-    
+    return null;
+}
